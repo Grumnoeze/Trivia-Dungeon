dc7e46e6a0652501cc560a896e73d1ea61882edc
Bruno Ezequiel Sánchez <3zequielbsanchez@gmail.com>
2025-09-05 13:39:49 -0300

    solucion de conflicto con la conexionBD

--- controller/conexionBD.php
+++ controller/conexionBD.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli("localhost", "root", "", "trivia-dungeon");
+    $conn = new mysqli("localhost", "root", "", "triviadungeon");
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }   
