8fd854e70b6152c5a71f343fd22b4ee07ba617dc
Antu27-a <superantugoku@gmail.com>
2025-09-18 16:02:50 -0300

    Update click.js

--- view/javascript/click.js
+++ view/javascript/click.js
@@ -520,7 +520,7 @@ function collidesWithWallAtPixel(px, py) {
   }
 
   // halfSize = mitad del sprite * margen pequeño para evitar quedarse pegado
-  let halfSize = spriteFullSize * 0.5 * 1.55; // 0.95 = 5% de tolerancia (ajusta si quieres)
+  let halfSize = spriteFullSize * 0.5 * 1.50; // 0.95 = 5% de tolerancia (ajusta si quieres)
 
   let left = px - halfSize;
   let right = px + halfSize;
