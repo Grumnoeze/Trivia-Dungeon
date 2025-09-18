25ff49c48b2c00b2676f47434b97ed39583e9659
Bruno Ezequiel Sánchez <3zequielbsanchez@gmail.com>
2025-09-18 12:33:32 -0300

    prueba

--- index.php
+++ index.php
@@ -3,12 +3,13 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
+    
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Trivia Dungeon</title>
     <link rel="stylesheet" href="../assets/css/styles.css">
+
 </head>
 
 <body>
@@ -34,7 +35,6 @@ session_start();
             <?php if (isset($_SESSION['username'])): ?>
             <p>Bienvenido, <?= htmlspecialchars($_SESSION['username']) ?>!</p>
             <?php endif; ?>
-
         </div>
     </header>
     <main>
