7410eb102c71c84c02d75f4797edeee9af602fb3
mts-mlna <matiasezequielmolina2006@gmail.com>
2025-11-20 15:21:36 -0300

    fcestdrtuyghjknbvh cgdxteryugjkhbvcfgdrytuihjn bvchfyugjhb nvcdztserdytufgjhvb nvbcxfgdsrythfvmb

--- view/pages/game.php
+++ view/pages/game.php
@@ -8,10 +8,11 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <title>Document</title>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/addons/p5.sound.min.js"></script>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/p5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/addons/p5.sound.min.js"></script>
 <script src="../javascript/click.js"></script>
+
 
 
 
