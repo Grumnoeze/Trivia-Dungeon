b796f915f5c883697440ab20133a54486b4fdc0b
mts-mlna <matiasezequielmolina2006@gmail.com>
2025-11-20 15:07:15 -0300

    CAllamU

--- view/pages/game.php
+++ view/pages/game.php
@@ -8,8 +8,8 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <title>Document</title>
-    <script src="../libraries/p5.js"></script>
-    <script src="../libraries/p5.sound.js"></script>
+    <script src="../libraries/p5.min.js"></script>
+    <script src="../libraries/p5.sound.min.js"></script>
 
     <script src="../javascript/click.js"></script>
 
