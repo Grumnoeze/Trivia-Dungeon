e3eaf402268ea0ef4d6407cfed157bf324d26cca
mts-mlna <matiasezequielmolina2006@gmail.com>
2025-11-20 14:49:57 -0300

    a

--- view/pages/game.php
+++ view/pages/game.php
@@ -8,9 +8,9 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <title>Document</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/addons/p5.sound.min.js"></script>
     <script language="javascript" type="text/javascript" src="../javascript/click.js"></script>
-    <script language="javascript" type="text/javascript" src="../libraries/p5.min.js"></script>
-    <script language="javascript" type="text/javascript" src="../libraries/p5.sound.min.js"></script>
 
 </head>
 
